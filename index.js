13eae239d8264f6975098631edc438e4948dcb98
William Saar <williamsaar@outlook.com>
2022-04-05 12:57:20 +0200

    initial commit with basics for express endpoints

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const users = [
+  {
+    name: "William",
+    age: 22,
+  },
+  {
+    name: "Tomas",
+    age: 69,
+  },
+];
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.put("/users", (req, res) => {
+  res.send("PUT request");
+});
+
+app.post("/users", (req, res) => {
+  users.push(req.body);
+  res.status(201);
+  res.send("Användare tillagd");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("Delete request");
+});
+
+app.listen(port, () =>
+  console.log(`App is running at http://localhost:${port}`)
+);
