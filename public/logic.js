e70545230d8dd2eeac6201f5b8d36bcb2c1079b4
William Saar <williamsaar@outlook.com>
2022-04-06 11:02:55 +0200

    Fixed bootstrap design

--- public/logic.js
+++ public/logic.js
@@ -14,20 +14,55 @@ const getCats = async () => {
 
 const renderCats = (cats) => {
   catContainer = document.getElementById("cat-container");
+  catContainer.innerHTML = null;
 
   for (let cat of cats.cats) {
-    let element = document.createElement("div");
+    let catLayout = `<div class="col">
+              <div class="card shadow-sm">
+                <svg
+                  class="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    ${cat.name}
+                  </text>
+                </svg>
 
-    let catName = document.createElement("h3");
-    catName.innerText = "Name: " + cat.name;
-    let catAge = document.createElement("p");
-    let catFood = document.createElement("p");
-    catFood.innerText = "Favorite food: " + cat.favoriteFood;
-    catAge.innerText = "Age: " + cat.age;
+                <div class="card-body">
+                  <p class="card-text">
+                    This cats' name is ${cat.name} and he/she is ${cat.age} years old. ${cat.name}'s favorite food is ${cat.favoriteFood}
+                  </p>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div class="btn-group">
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>`;
 
-    catContainer.appendChild(element);
-    element.appendChild(catName);
-    element.appendChild(catAge);
-    element.append(catFood);
+    catContainer.innerHTML = catContainer.innerHTML + catLayout;
   }
 };
