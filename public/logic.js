fdd7b884b765004d0b0fcc8083c50c067cc2583f
William Saar <williamsaar@outlook.com>
2022-04-08 17:11:44 +0200

    Cats can now be added and contain an imgae url

--- public/logic.js
+++ public/logic.js
@@ -2,6 +2,9 @@ window.addEventListener("load", (event) => {
   document
     .getElementById("button")
     .addEventListener("click", async () => getCats());
+  document
+    .getElementById("add-a-cat")
+    .addEventListener("click", () => renderPostCat());
 });
 
 const getCats = async () => {
@@ -25,6 +28,10 @@ const getCat = async (id) => {
     <label for="cat-favorite-food" class="form-label">Favorite food</label>
     <input class="form-control" id="cat-favorite-food" aria-labelledby="cat-favorite-food" name="cat-favorite-food" value="${singleCat.favoriteFood}">
   </div>
+  <div class="mb-3">
+    <label for="cat-url" class="form-label">Image URL</label>
+    <input class="form-control" id="cat-url" aria-labelledby="cat-url" name="cat-url" value="${singleCat.url}">
+  </div>
  
   <button type="submit" class="btn btn-primary" id="submit${id}">Submit</button>
 </form>`;
@@ -41,11 +48,13 @@ const updateCat = async (event, catId, catElement) => {
   let catName = event.target[0].value;
   let catAge = event.target[1].value;
   let catFavoriteFood = event.target[2].value;
+  let catUrl = event.target[3].value;
 
   let object = {
     name: catName,
     age: parseInt(catAge),
     favoriteFood: catFavoriteFood,
+    url: catUrl,
     id: catId,
   };
 
@@ -56,6 +65,69 @@ const updateCat = async (event, catId, catElement) => {
   );
 
   catElement.innerHTML = `<h3>${result}<h3>`;
+
+  //To run the code after 2 seconds
+  setTimeout(function () {
+    getCats();
+  }, 2000);
+};
+
+const renderPostCat = () => {
+  const catContainer = document.getElementById("cat-container");
+  catContainer.innerHTML =
+    catContainer.innerHTML +
+    `
+    <div class="col" id="catNew">
+        <form id="addCat">
+            <div class="mb-3">
+                <label for="cat-name" class="form-label">Cat name</label>
+                <input class="form-control" id="cat-name" aria-labelledby="cat-name" name="cat-name">
+            </div>
+            <div class="mb-3">
+                <label for="cat-age" class="form-label">Age</label>
+                <input type="number" class="form-control" id="cat-age" aria-labelledby="cat-age" name="cat-age">
+            </div>
+            <div class="mb-3">
+                <label for="cat-favorite-food" class="form-label">Favorite food</label>
+                <input class="form-control" id="cat-favorite-food" aria-labelledby="cat-favorite-food" name="cat-favorite-food">
+            </div>
+            <div class="mb-3">
+                <label for="cat-url" class="form-label">Image URL</label>
+                <input class="form-control" id="cat-url" aria-labelledby="cat-url" name="cat-url">
+            </div>
+            <button type="submit" class="btn btn-primary" id="submit">Add cat</button>
+        </form> 
+    </div>`;
+
+  let form = document.getElementById("addCat");
+  form.addEventListener("submit", async (event) => {
+    postCat(event);
+  });
+};
+
+const postCat = async (event) => {
+  event.preventDefault();
+
+  let catName = event.target[0].value;
+  let catAge = event.target[1].value;
+  let catFavoriteFood = event.target[2].value;
+  let catUrl = event.target[3].value;
+
+  let object = {
+    name: catName,
+    age: parseInt(catAge),
+    favoriteFood: catFavoriteFood,
+    url: catUrl,
+    id: "",
+  };
+
+  let result = await makeRequest(
+    `http://localhost:8080/api/cats/`,
+    "POST",
+    object
+  );
+
+  document.getElementById("catNew").innerHTML = `<h3>${result}<h3>`;
 
   //To run the code after 2 seconds
   setTimeout(function () {
@@ -98,32 +170,14 @@ const renderCats = (cats) => {
 
   for (let cat of cats) {
     let catLayout = `<div class="col" id="cat${cat.id}">
-              <div class="card shadow-sm">
-                <svg
-                  class="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="225"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: Thumbnail"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>${cat.name}</title>
-                  <rect width="100%" height="100%" fill="#55595c"></rect>
-                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                    ${cat.name}
-                  </text>
-                </svg>
-
-                <div class="card-body">
-                  <p class="card-text">
-                    This cats' name is ${cat.name} and he/she is ${cat.age} years old. ${cat.name}'s favorite food is ${cat.favoriteFood}
-                  </p>
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div >
+        <div class="card shadow-sm">
+            <img src="${cat.url}" class="card-img-top" alt="${cat.name}">
+            <div class="card-body">
+                    <p class="card-text">
+                        This cats' name is ${cat.name} and he/she is ${cat.age} years old. ${cat.name}'s favorite food is ${cat.favoriteFood}.
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                    <div>
                       <button
                         type="button"
                         class="btn btn-sm btn-danger"
@@ -140,10 +194,10 @@ const renderCats = (cats) => {
                       </button>
                     </div>
                     <small class="text-muted">Cat-ID: ${cat.id}</small>
-                  </div>
                 </div>
-              </div>
-            </div>`;
+            </div>
+        </div>
+    </div>`;
 
     catContainer.innerHTML = catContainer.innerHTML + catLayout;
   }
